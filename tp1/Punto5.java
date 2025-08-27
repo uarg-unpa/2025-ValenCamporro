9b64043519add3aa2b0edc4d6d19710db0af5fc6
ValenCamporro <164282443+ValenCamporro@users.noreply.github.com>
2025-08-27 18:46:08 +0000

    Terminado

--- tp1/Punto5.java
+++ tp1/Punto5.java
@@ -10,7 +10,18 @@ public class Punto5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese un día de la semana (L, M, I, J, V, S, D)");
+        char dia = scanner.next().charAt(0);
 
-        
+        if (dia == 'M' || dia == 'I' || dia == 'J') {
+            System.out.println("HOY TENGO RESOLUCION");
+        } else {
+            if (dia == 'L' || dia == 'V' || dia == 'S' || dia == 'D') {
+                System.out.println("HOY NO TENGO RESOLUCION");
+            } else {
+                System.out.println("Día invalido. Intentar de nuevo.");
+            }
+        }
+        scanner.close();
     }
 }
---
+++ tp1/Punto6.java
@@ -0,0 +1,27 @@
+package tp1;
+import java.util.Scanner;
+
+/* Leer tres números enteros dentro de las variables NUM1, NUM2 y NUM3 respectivamente y calcular e
+imprimir el producto y la suma de ellos. */
+
+public class Punto6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese tres números enteros.");
+        System.out.println("Número 1: ");
+        int NUM1 = scanner.nextInt();
+        System.out.println("Número 2: ");
+        int NUM2 = scanner.nextInt();
+        System.out.println("Número 3: ");
+        int NUM3 = scanner.nextInt();
+
+        int producto = NUM1 * NUM2 * NUM3;
+        int suma = NUM1 + NUM2 + NUM3;
+
+        System.out.println("El producto de los números ingresados: " + producto);
+        System.out.println("La suma de los números ingresados: " + suma);
+
+        scanner.close();
+    }
+}
