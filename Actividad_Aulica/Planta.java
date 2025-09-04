47aca51b1cea517dc835bc0398be06b045bfe2b7
ValenCamporro <164282443+ValenCamporro@users.noreply.github.com>
2025-09-04 21:30:17 +0000

    Terminamos Actividad Aulica

---
+++ Actividad_Aulica/Planta.java
@@ -0,0 +1,58 @@
+package Actividad_Aulica;
+
+public class Planta {
+
+    private String nombre;
+    private String tipo;
+    private int nivelAgua;
+    private double altura; 
+
+    public Planta (String nombre, String tipo, int nivelAgua, double altura) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.nivelAgua = nivelAgua;
+        this.altura = altura;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+     public int getNivelAgua(){
+        return nivelAgua;
+    }
+    public double getAltura(){
+        return altura;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    public void setNivelAgua(int nivelAgua ){
+        this.nivelAgua = nivelAgua;
+    }
+    public void setAltura (double altura){
+        this.altura = altura;
+    }
+
+    public String verificarRiego() {
+        if (nivelAgua < 30) {
+            return "La planta necesita ser regada";
+        } else {
+            return "La planta no necesita ser regada";
+        }
+    }
+
+    public void crecer(double crecimiento){
+        altura = altura + crecimiento;
+    }
+
+    public String toString() {
+        return "Planta: " + nombre + " - Tipo de planta: " + tipo + " - Altura: " + altura + " cm - Agua: " + nivelAgua + "% - Estado: " + verificarRiego();
+    }
+}
