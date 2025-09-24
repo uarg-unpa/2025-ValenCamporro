60e935ddac9a27ecae0ebd3f532f8c48ac8d0cd3
ValenCamporro <164282443+ValenCamporro@users.noreply.github.com>
2025-09-24 00:22:55 +0000

    Terminado tp2_2

---
+++ tp3/Punto3/palabraInversa.java
@@ -0,0 +1,11 @@
+package Punto3;
+
+public class palabraInversa {
+    
+    private
+
+    String palabra = "Programación";
+    for (int i=0, i<palabra.lenght(); i++){
+        System.out.println(palabra.charAt(i));
+    }
+}
